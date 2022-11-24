06858dc4edd54e97a856e05c6dbbdac9ceb0e74c
rajesh6334 <118992210+rajesh6334@users.noreply.github.com>
2022-11-24 20:21:25 +0530

    Add files via upload

---
+++ arthemetic operations.py
@@ -0,0 +1,18 @@
+#arthemetic operations
+print('enter the values:')
+a=int(input())
+b=int(input())
+c=a+b
+print('addition:',c)
+c=a-b
+print('subtraction:',c)
+c=a*b
+print('multiplucation:',c)
+c=a/b
+print('divition:',c)
+c=a%b
+print('modulo division:',c)
+c=a//b
+print('floor division:',c)
+c=a**b
+print('power:',c)
